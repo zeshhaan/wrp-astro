6ebb73837554ff39242f690b1d4eeb575bc7a6e0
Mohammed Zeeshan <work.zeeshan@gmail.com>
2025-10-30 20:27:33 +0400

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.54.0
      project name = wrp-astro
      framework = astro
      framework cli = create-astro@4.13.1
      package manager = bun@1.3.0
      wrangler = wrangler@4.45.0
      git = 2.46.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    },
+
+    imageService: "cloudflare"
+  }),
 });
